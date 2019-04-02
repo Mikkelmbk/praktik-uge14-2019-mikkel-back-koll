8796e03b2e759fd8382da4ed83ba4294a0d17ff9
Mikkel Back Koll <217236@RTS-365.dk>
2019-04-02 11:42:23 +0200

    pushede objekter til array med title og url data

--- dist/js/underside1.js
+++ dist/js/underside1.js
@@ -8,24 +8,36 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 		galleryContainerElement.style.display = "flex";
 		galleryContainerElement.style.flexDirection = "column";
+		galleryContainerElement.style.alignItems = "center";
 
 		let imageIndex = 0;
 		let currentImageElement = galleryContainerElement.querySelector('#current-image');
 		let dataArray = [];
 		let nextSlideBtnElement = galleryContainerElement.querySelector('#btn-next-image');
 		let prevSlideBtnElement = galleryContainerElement.querySelector('#btn-prev-image');
+		let imageCountElement = galleryContainerElement.querySelector('.imageCount');
+		let imageTitleElement = galleryContainerElement.querySelector('.imageTitle');
 
+
+
+		
 		dataUrlElements.forEach((dataUrlElement)=>{
-			dataArray.push(dataUrlElement.dataset.url);
+	
+			dataArray.push({url:dataUrlElement.dataset.url, title:dataUrlElement.dataset.title});
+
 		})
 
+
+
 		updateSource();
+		imageCount();
 
 		nextSlideBtnElement.addEventListener('click',()=>{
 			imageIndex++;
 			if(imageIndex >= dataArray.length){
 				imageIndex = 0;
 			}
+			imageCount();
 			updateSource();
 		});
 
@@ -34,6 +46,7 @@ document.addEventListener('DOMContentLoaded',()=>{
 			if(imageIndex < 0){
 				imageIndex = dataArray.length -1;
 			}
+			imageCount();
 			updateSource();
 		})
 
@@ -41,11 +54,16 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 		function updateSource(){
 			if(dataArray.length != 0){
-				currentImageElement.src = dataArray[imageIndex];
+				currentImageElement.src = dataArray[imageIndex].url;
 				}
 		}
 
+		function imageCount(){
+			imageCountElement.innerHTML = imageIndex+1 + "/" + dataArray.length;
+			imageTitleElement.innerHTML = dataArray[imageIndex].title;
+		}
 	}
+
 
 
 }); // DOMContentLoaded Slutter
