8240ba61aea9daed457a1eb33e62f96ea583c5b1
Mikkel Back Koll <217236@RTS-365.dk>
2019-04-03 13:01:38 +0200

    preventDefault hvis krav ikke opfyldt

--- dist/js/underside3.js
+++ dist/js/underside3.js
@@ -1,40 +1,78 @@
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
 	let formElement = document.querySelector('.form');
 	let guestBookElement = document.querySelector('.guestBook');
 	let guestBookArray = [];
-	if(localStorage.getItem('guestBookData')){
+	if (localStorage.getItem('guestBookData')) {
 		guestBookArray = JSON.parse(localStorage.getItem('guestBookData'));
+	}
+	let validateRequirements = 0;
 
-		console.log(guestBookArray);
+
+	formElement.addEventListener('submit', (event) => {
+
+		validateCounter(event);
+
+	})
+	function confirmValidation(event) {
+
+
+		if (validateRequirements == 3) {
+
+			guestBookArray.push({ name: event.target.name.value, email: event.target.email.value, message: event.target.message.value });
+
+			let guestBookArrayStringified = JSON.stringify(guestBookArray);
+
+			localStorage.setItem('guestBookData', guestBookArrayStringified);
+		}
+		else{
+			event.preventDefault();
+		}
 	}
 
-	formElement.addEventListener('submit',(event)=>{
 
-		guestBookArray.push({name:event.target.name.value, email:event.target.email.value, message:event.target.message.value});
-	
-		let guestBookArrayStringified = JSON.stringify(guestBookArray);
 
-		localStorage.setItem('guestBookData',guestBookArrayStringified);
-	})
-
-	guestBookArray.forEach((guestBookCredentials,index)=>{
+	guestBookArray.forEach((guestBookCredentials, index) => {
 		console.log(guestBookCredentials);
+
+		let guestBookboxElement = document.createElement('div');
+		guestBookboxElement.classList.add('guestBookSeperator');
+		guestBookElement.appendChild(guestBookboxElement);
+
 		let guestNameElement = document.createElement('h3');
-		guestBookElement.appendChild(guestNameElement);
+		guestBookboxElement.appendChild(guestNameElement);
 		guestNameElement.innerHTML = guestBookArray[index].name;
 
 		let guestEmailElement = document.createElement('h4');
-		guestBookElement.appendChild(guestEmailElement);
+		guestBookboxElement.appendChild(guestEmailElement);
 		guestEmailElement.innerHTML = guestBookArray[index].email;
 
 		let guestMessageElement = document.createElement('p');
-		guestBookElement.appendChild(guestMessageElement);
+		guestBookboxElement.appendChild(guestMessageElement);
 		guestMessageElement.innerHTML = guestBookArray[index].message;
 	})
 
 
 
+	function validateCounter(event) {
+		if (event.target.name.value != "" && isNaN(event.target.name.value)) {
+			validateRequirements++;
 
+		}
+		if (event.target.email.value != "" && validateEmail(event.target.email.value)) {
+			validateRequirements++;
+		
+		}
+		if (event.target.message.value != "") {
+			validateRequirements++;
+		
+		}
 
+		confirmValidation(event);
+		validateRequirements = 0;
+	}
+
+	function validateEmail(email) {
+		let re = /(.+)@(.+){2,}\.(.+){2,}$/; return re.test(String(email).toLowerCase());
+	}
 }); // DOMContentLoaded Slutter
